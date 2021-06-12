ce4a02c2546626e1b6260e8159ea240455217b27
markopolo139 <marekseget@onet.pl>
2021-06-12 17:41:55 +0200

    feat(business): create call value

---
+++ src/main/java/dev/mk/First/business/value/Call.java
@@ -0,0 +1,80 @@
+package dev.mk.First.business.value;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.Objects;
+
+public class Call {
+
+    private LocalDateTime mStartDateTime;
+    private LocalDateTime mEndDateTime;
+    private CallType mCallType;
+    private String mLocation;
+    private String mDescription;
+    private Collection<FireTruck> mFireTrucksInAction;
+    private Collection<StaffMember> mStaffMembersInAction;
+
+    public LocalDateTime getStartDateTime() {
+        return mStartDateTime;
+    }
+
+    public LocalDateTime getEndDateTime() {
+        return mEndDateTime;
+    }
+
+    public CallType getCallType() {
+        return mCallType;
+    }
+
+    public String getLocation() {
+        return mLocation;
+    }
+
+    public String getDescription() {
+        return mDescription;
+    }
+
+    public Collection<FireTruck> getFireTrucksInAction() {
+        return mFireTrucksInAction;
+    }
+
+    public Collection<StaffMember> getStaffMembersInAction() {
+        return mStaffMembersInAction;
+    }
+
+    public Call(
+            LocalDateTime startDateTime, LocalDateTime endDateTime, CallType callType, String location,
+            String description, Collection<FireTruck> fireTrucksInAction, Collection<StaffMember> staffMembersInAction)
+    {
+        mStartDateTime = startDateTime;
+        mEndDateTime = endDateTime;
+        mCallType = callType;
+        mLocation = location;
+        mDescription = description;
+        mFireTrucksInAction = fireTrucksInAction;
+        mStaffMembersInAction = staffMembersInAction;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Call call = (Call) o;
+        return getStartDateTime().equals(call.getStartDateTime())
+                && getEndDateTime().equals(call.getEndDateTime())
+                && getCallType() == call.getCallType()
+                && getLocation().equals(call.getLocation())
+                && Objects.equals(getDescription(), call.getDescription())
+                && getFireTrucksInAction().equals(call.getFireTrucksInAction())
+                && getStaffMembersInAction().equals(call.getStaffMembersInAction());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(
+                getStartDateTime(), getEndDateTime(), getCallType(), getLocation(), getDescription(),
+                getFireTrucksInAction(), getStaffMembersInAction()
+        );
+    }
+}
---
+++ src/main/java/dev/mk/First/business/value/CallType.java
@@ -0,0 +1,9 @@
+package dev.mk.First.business.value;
+
+public enum CallType {
+    FIRE,
+    LOCAL_THREAT,
+    TRAINING,
+    FALSE_ALARM,
+    SECURE_REGION
+}
