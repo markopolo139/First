36a24d09acba1bbd5b4e5e0edc5ad41141b139d2
markopolo139 <marekseget@onet.pl>
2021-06-16 21:10:56 +0200

    feat(configuration): create configuration for thymeleaf

---
+++ src/main/java/dev/mk/First/app/configuration/ThyMeLeafConfiguration.java
@@ -0,0 +1,36 @@
+package dev.mk.First.app.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.thymeleaf.ITemplateEngine;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+
+@Configuration
+public class ThyMeLeafConfiguration {
+
+    @Bean
+    @Scope("Singleton")
+    public ITemplateResolver getTemplateResolver() {
+        ClassLoaderTemplateResolver classLoaderTemplateResolver = new ClassLoaderTemplateResolver();
+        classLoaderTemplateResolver.setTemplateMode(TemplateMode.HTML);
+//        classLoaderTemplateResolver.setPrefix("/templates/");
+//        classLoaderTemplateResolver.setSuffix(".html");
+//        classLoaderTemplateResolver.setCharacterEncoding("UTF-8");
+        return classLoaderTemplateResolver;
+    }
+
+    @Bean
+    @Scope("Singleton")
+    public ITemplateEngine getTemplateEngine() {
+        TemplateEngine templateEngine = new TemplateEngine();
+        templateEngine.setTemplateResolver(getTemplateResolver());
+        templateEngine.addDialect(new Java8TimeDialect());
+        
+        return templateEngine;
+    }
+}
