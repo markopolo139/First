2214dcbee820d98a097e1b1f2863f99a5080bc1f
Brys49 <krzysztof4499@gmail.com>
2021-06-29 17:41:44 +0200

    feat(FireTruckParameterConverter): implemented converter

--- src/main/java/dev/mk/First/app/converters/FireTruckParameterConverter.java
+++ src/main/java/dev/mk/First/app/converters/FireTruckParameterConverter.java
@@ -1,7 +1,20 @@
 package dev.mk.First.app.converters;
 
+import dev.mk.First.app.data.entities.FireTruckParameterEntity;
+import dev.mk.First.web.models.FireTruckParameterModel;
 import org.springframework.stereotype.Component;
+
+import java.util.Collection;
 
 @Component
 public interface FireTruckParameterConverter {
+
+    FireTruckParameterModel convertToModel(FireTruckParameterEntity fireTruckParameterEntity);
+
+    FireTruckParameterEntity convertToEntity(FireTruckParameterModel fireTruckParameterModel);
+
+    Collection<FireTruckParameterModel> convertCollectionToModel(Collection<FireTruckParameterEntity> fireTruckParameterEntities);
+
+    Collection<FireTruckParameterEntity> convertCollectionToEntity(Collection<FireTruckParameterModel> fireTruckParameterModels);
+
 }
---
+++ src/main/java/dev/mk/First/app/converters/FireTruckParameterConverterImpl.java
@@ -0,0 +1,34 @@
+package dev.mk.First.app.converters;
+
+import dev.mk.First.app.data.entities.FireTruckParameterEntity;
+import dev.mk.First.web.models.FireTruckParameterModel;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+public class FireTruckParameterConverterImpl implements FireTruckParameterConverter{
+
+    @Override
+    public FireTruckParameterModel convertToModel(FireTruckParameterEntity fireTruckParameterEntity) {
+        return new FireTruckParameterModel(fireTruckParameterEntity.parameter, fireTruckParameterEntity.value);
+    }
+
+    @Override
+    public FireTruckParameterEntity convertToEntity(FireTruckParameterModel fireTruckParameterModel) {
+        return new FireTruckParameterEntity(fireTruckParameterModel.parameter, fireTruckParameterModel.value);
+    }
+
+    @Override
+    public Collection<FireTruckParameterModel> convertCollectionToModel(
+            Collection<FireTruckParameterEntity> fireTruckParameterEntities
+    ) {
+        return fireTruckParameterEntities.stream().map(this::convertToModel).collect(Collectors.toList());
+    }
+
+    @Override
+    public Collection<FireTruckParameterEntity> convertCollectionToEntity(
+            Collection<FireTruckParameterModel> fireTruckParameterModels
+    ) {
+        return fireTruckParameterModels.stream().map(this::convertToEntity).collect(Collectors.toList());
+    }
+}
