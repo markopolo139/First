da56774ca457e356d27ba85fc66aec2fd4603ad1
markopolo139 <marekseget@onet.pl>
2021-06-30 15:46:01 +0200

    refractor(equipmentController): rename mappings

--- src/main/java/dev/mk/First/web/controllers/EquipmentController.java
+++ src/main/java/dev/mk/First/web/controllers/EquipmentController.java
@@ -16,27 +16,27 @@ public class EquipmentController {
     @Autowired
     private EquipmentInteractor mEquipmentInteractor;
 
-    @GetMapping(path = "/api/v1/getEquipment")
+    @GetMapping(path = "/api/v1/equipment/get")
     public Collection<EquipmentEntity> getEquipment() {
         return mEquipmentInteractor.findAll();
     }
 
-    @PostMapping(path = "/api/v1/addEquipment")
+    @PostMapping(path = "/api/v1/equipment/add")
     public void addEquipment(@Valid @RequestBody EquipmentModel equipmentModel) {
         mEquipmentInteractor.saveEntity(equipmentModel);
     }
 
-    @DeleteMapping(path = "/api/v1/deleteEquipment")
+    @DeleteMapping(path = "/api/v1/equipment/deleteById")
     public void deleteEquipmentById(Integer id) {
         mEquipmentInteractor.deleteEntity(id);
     }
 
-    @DeleteMapping(path = "/api/v1/deleteEquipment")
+    @DeleteMapping(path = "/api/v1/equipment/deleteByIds")
     public void deleteEquipmentByIdCollection(Collection<Integer> id) {
         mEquipmentInteractor.deleteEntities(id);
     }
 
-    @PatchMapping(path = "/api/v1/updateEquipment")
+    @PatchMapping(path = "/api/v1/equipment/update")
     public void updateEquipment(Integer id, @Valid @RequestBody EquipmentModel updatedEquipment) {
         mEquipmentInteractor.updateEntity(id,updatedEquipment);
     }
