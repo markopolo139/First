6c894aea3a99d6fee781bf00212045308146b526
Brys49 <krzysztof4499@gmail.com>
2021-06-30 15:55:51 +0200

    feat(controllers): created controllers and interactors for FireTruck

--- src/main/java/dev/mk/First/app/interactors/FireTruckInteractor.java
+++ src/main/java/dev/mk/First/app/interactors/FireTruckInteractor.java
@@ -1,12 +1,45 @@
 package dev.mk.First.app.interactors;
 
+import dev.mk.First.app.converters.FireTruckConverter;
+import dev.mk.First.app.data.entities.FireTruckEntity;
 import dev.mk.First.app.data.repositories.FireTruckRepository;
+import dev.mk.First.web.models.FireTruckModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Collection;
 
 @Service
 public class FireTruckInteractor {
 
     @Autowired
     private FireTruckRepository mFireTruckRepository;
+
+    @Autowired
+    private FireTruckConverter mFireTruckConverter;
+
+    public Collection<FireTruckEntity> findAll() {
+        return mFireTruckRepository.findAll();
+    }
+
+    public void saveEntity(FireTruckModel fireTruckModel) {
+        mFireTruckRepository.save(mFireTruckConverter.convertToEntity(fireTruckModel));
+    }
+
+    public void deleteEntity(Integer id) {
+        mFireTruckRepository.deleteById(id);
+    }
+
+    public void deleteEntities(Collection<Integer> id) {
+        mFireTruckRepository.deleteAllById(id);
+    }
+
+    public void updateEntity(Integer id, FireTruckModel updatedFireTruck) {
+
+        mFireTruckRepository.deleteById(id);
+        FireTruckEntity updatedEntity = mFireTruckConverter.convertToEntity(updatedFireTruck);
+        updatedEntity.fireTruckId = id;
+        mFireTruckRepository.save(updatedEntity);
+
+    }
 }
--- src/main/java/dev/mk/First/web/controllers/FireTruckController.java
+++ src/main/java/dev/mk/First/web/controllers/FireTruckController.java
@@ -1,9 +1,43 @@
 package dev.mk.First.web.controllers;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import dev.mk.First.app.data.entities.FireTruckEntity;
+import dev.mk.First.app.interactors.FireTruckInteractor;
+import dev.mk.First.web.models.FireTruckModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.Collection;
 
 @RestController
 @CrossOrigin
 public class FireTruckController {
+
+    @Autowired
+    private FireTruckInteractor mFireTruckInteractor;
+
+    @GetMapping(path = "/api/v1/fireTruck/get")
+    public Collection<FireTruckEntity> getFireTruck() {
+        return mFireTruckInteractor.findAll();
+    }
+
+    @PostMapping(path = "/api/v1/fireTruck/add")
+    public void addFireTruck(@Valid @RequestBody FireTruckModel fireTruckModel) {
+        mFireTruckInteractor.saveEntity(fireTruckModel);
+    }
+
+    @DeleteMapping(path = "/api/v1/fireTruck/deleteById")
+    public void deleteFireTruckById(Integer id) {
+        mFireTruckInteractor.deleteEntity(id);
+    }
+
+    @DeleteMapping(path = "/api/v1/fireTruck/deleteByIds")
+    public void deleteFireTruckByIdCollection(Collection<Integer> id) {
+        mFireTruckInteractor.deleteEntities(id);
+    }
+
+    @PatchMapping(path = "/api/v1/fireTruck/update")
+    public void updateFireTruck(Integer id, @Valid @RequestBody FireTruckModel updatedFireTruck) {
+        mFireTruckInteractor.updateEntity(id,updatedFireTruck);
+    }
 }
