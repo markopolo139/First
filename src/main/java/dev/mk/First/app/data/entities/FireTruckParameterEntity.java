ffe62b9285f4e1837e2a38e888c8122125a9e8db
markopolo139 <marekseget@onet.pl>
2021-06-30 16:53:57 +0200

    feat(entity): implement equals to fireTruckParameters

--- src/main/java/dev/mk/First/app/data/entities/FireTruckParameterEntity.java
+++ src/main/java/dev/mk/First/app/data/entities/FireTruckParameterEntity.java
@@ -1,6 +1,7 @@
 package dev.mk.First.app.data.entities;
 
 import javax.persistence.Embeddable;
+import java.util.Objects;
 
 @Embeddable
 public class FireTruckParameterEntity {
@@ -17,4 +18,16 @@ public class FireTruckParameterEntity {
         this.value = value;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        FireTruckParameterEntity that = (FireTruckParameterEntity) o;
+        return Objects.equals(parameter, that.parameter) && Objects.equals(value, that.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(parameter, value);
+    }
 }
