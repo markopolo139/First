a1fdc29fb7461ad05e4cf0ccc39021b7ee739ddb
markopolo139 <marekseget@onet.pl>
2021-06-30 16:01:46 +0200

    test: create test structure

---
+++ src/test/java/dev/mk/First/app/repository/CallRepositoryMockUp.java
@@ -0,0 +1,8 @@
+package dev.mk.First.app.repository;
+
+import dev.mk.First.app.data.repositories.CallRepository;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CallRepositoryMockUp implements CallRepository {
+}
