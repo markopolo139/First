494bccf7ee0411a3719d87f1b7e3955e8b196c13
markopolo139 <marekseget@onet.pl>
2021-07-20 20:14:39 +0200

    refactor(error): correct spelling errors in error handler

--- src/main/java/dev/mk/First/web/error/ErrorHandler.java
+++ src/main/java/dev/mk/First/web/error/ErrorHandler.java
@@ -11,6 +11,7 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintViolationException;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -24,22 +25,14 @@ public class ErrorHandler extends ResponseEntityExceptionHandler {
         return new ResponseEntity<>(apiError, apiError.httpStatus);
     }
 
-    @ExceptionHandler
-    public ResponseEntity<Object> exceptionHandle(Exception ex) {
-        ApiError apiError = ApiError.builder()
-                .setHttpStatus(HttpStatus.BAD_REQUEST).build();
-
-        return convertToResponse(apiError);
-    }
-
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(
             MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request
     ) {
         List<ApiSubError> apiSubErrors = ex.getBindingResult().getFieldErrors().stream().map(
                 i -> new ApiSubError(
-                        "Type valid value",
-                        "Not valid value" + i.getRejectedValue() + "-" + i.getDefaultMessage()
+                        "Type valid value for " + i.getField(),
+                        "Not valid value " + i.getRejectedValue() + " - " + i.getDefaultMessage()
                 )
         ).collect(Collectors.toList());
 
@@ -49,6 +42,24 @@ public class ErrorHandler extends ResponseEntityExceptionHandler {
                 .setHttpStatus(HttpStatus.BAD_REQUEST)
                 .setApiSubErrors(apiSubErrors)
                 .build();
+        return convertToResponse(apiError);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<Object> constraintViolationExceptionHandler(ConstraintViolationException exception) {
+        List<ApiSubError> apiSubErrors = exception.getConstraintViolations().stream()
+                .map(i -> new ApiSubError(
+                                "Check the rejected value",
+                                "Validation failed for" + i.getInvalidValue() + " - " + i.getMessage()
+                        )
+                ).collect(Collectors.toList());
+
+        ApiError apiError = ApiError.builder().setApiSubErrors(apiSubErrors)
+                .setErrorMessage("Validation failed")
+                .setDefaultAction("Check sub errors")
+                .setHttpStatus(HttpStatus.BAD_REQUEST)
+                .build();
+
         return convertToResponse(apiError);
     }
 }
