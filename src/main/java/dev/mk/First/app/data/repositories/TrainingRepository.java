1f0b5c2a1190d1908e848be7f07e8ee6696f7aec
Brys49 <krzysztof4499@gmail.com>
2021-07-22 17:48:28 +0200

    feat(repositories): create filtering query for FireTruck and Trainig repos

--- src/main/java/dev/mk/First/app/data/repositories/TrainingRepository.java
+++ src/main/java/dev/mk/First/app/data/repositories/TrainingRepository.java
@@ -1,10 +1,30 @@
 package dev.mk.First.app.data.repositories;
 
 import dev.mk.First.app.data.entities.TrainingEntity;
+import dev.mk.First.business.value.TrainingType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Repository
 public interface TrainingRepository extends CrudRepository<TrainingEntity, Integer> {
+
+    @Query(
+            "select te from TrainingEntity te" +
+                    "where (:training_dateStart is null or te.trainingDate >= :training_dateStart)" +
+                    "and (:training_dateEnd is null or te.trainingDate <= :training_dateEnd)" +
+                    "and (:expiration_dateStart is null or te.expirationDate >= :expiration_dateStart)" +
+                    "and (:expiration_dateEnd is null or te.expirationDate <= :expiration_dateEnd)" +
+                    "and (:training_type is null or te.trainingType = :training_type)"
+    )
+    List<TrainingEntity> findAllByFilter(@Param("training_dateStart") LocalDate training_dateStart,
+                                         @Param("training_dateEnd") LocalDate training_dateEnd,
+                                         @Param("expiration_dateStart") LocalDate expiration_dateStart,
+                                         @Param("expiration_dateEnd") LocalDate expiration_dateEnd,
+                                         @Param("training_type") TrainingType training_type);
 }
